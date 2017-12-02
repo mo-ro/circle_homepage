e3e46cb1879fe1e5ad5794cedb11b698b503cb3c
mo-ro <gainmoro@gmail.com>
2017-12-02 23:04:27 +0900

    アニメーション修正

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(window).on('load', function(){
-  TweenMax.to(".top__image--up", .5, {top: 0, left: -300, opacity: 1});
-  TweenMax.to(".top__image--down", .5, {left: -300, bottom: -350, opacity: 1, delay: .3});
+  // TweenMax.to(".top__image--up", .5, {top: 0, left: -300, opacity: 1});
+  // TweenMax.to(".top__image--down", .5, {left: -300, bottom: -350, opacity: 1, delay: .3});
   TweenMax.from(".Line", 3, {transform: "rotate(300deg)"});
-  TweenMax.to(".top__image-inner--up, .top__image-inner--down, .top__right", 1, {opacity: 1, delay: 1});
+  TweenMax.to(".top__image-inner--up, .top__image-inner--down, .top__right", 1, {opacity: 1, delay: 1, ease: Power0.easeNone});
 });
 
